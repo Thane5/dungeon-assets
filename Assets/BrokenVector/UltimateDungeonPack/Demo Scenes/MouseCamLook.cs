718940a39bc7e3b093cd2be23f105fa6c404fbaa
Thane5 <13683581+Thane5@users.noreply.github.com>
2022-01-27 21:28:11 +0100

    Fixed Player Scripts, added Assets for Oven, Food , Dishes

--- Assets/BrokenVector/UltimateDungeonPack/Demo Scenes/MouseCamLook.cs
+++ Assets/BrokenVector/UltimateDungeonPack/Demo Scenes/MouseCamLook.cs
@@ -1,46 +1,28 @@
-﻿/* 
- * author : jiankaiwang
- * description : The script provides you with basic operations 
- *               of first personal camera look on mouse moving.
- * platform : Unity
- * date : 2017/12
- */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseCamLook : MonoBehaviour {
+public class MouseCamLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
 
-    [SerializeField]
-    public float sensitivity = 5.0f;
-    [SerializeField]
-    public float smoothing = 2.0f;
-    // the chacter is the capsule
-    public GameObject character;
-    // get the incremental value of mouse moving
-    private Vector2 mouseLook;
-    // smooth the mouse moving
-    private Vector2 smoothV;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
-	// Use this for initialization
-	void Start () {
-        character = this.transform.parent.gameObject;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        // md is mosue delta
-        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
-        // the interpolated float result between the two float values
-        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
-        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
-        // incrementally add to the camera look
-        mouseLook += smoothV;
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        // vector3.right means the x-axis
-        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
-        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
